304af307dc9cc2efd23de209dddc2b516032bb7b
esalo12 <espen.salomonsen@student.nord.no>
2016-11-24 00:20:55 +0100

    brukerid på foto

--- src/main/java/com/example/FotoController.java
+++ src/main/java/com/example/FotoController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +16,8 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.stream.Collectors;
 
 import static org.codehaus.groovy.runtime.DefaultGroovyMethods.collect;
@@ -31,6 +33,8 @@ public class FotoController{
     }
     @Autowired
     FotoRepository fotoRepository;
+    @Autowired
+    FotografRepository fotografRepository;
 
     @GetMapping("/")
     public String listUploadedFiles(Model model) throws IOException {
@@ -42,14 +46,14 @@ public class FotoController{
                                 .build().toString())
                 .collect(Collectors.toList()));
 
-        return "rediger";
+        return "home";
 
     }
     @GetMapping("/files/{filename:.+}")
     @ResponseBody
     public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
 
-        Resource file = storageService.loadAsResource(filename);
+        Resource file = storageService.loadAsResource(filename+".jpg");
         return ResponseEntity
                 .ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+file.getFilename()+"\"")
@@ -57,12 +61,17 @@ public class FotoController{
     }
 
     @PostMapping("/")
-    public String handleFileUpload(@RequestParam("file")MultipartFile file,
-                                   RedirectAttributes redirectAttributes) {
+    public String handleFileUpload(@RequestParam("file")MultipartFile file, @RequestParam("tittel") String tittel,
+                                   RedirectAttributes redirectAttributes) throws IOException {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Foto f = new Foto();
-
+        Fotograf fotgraf = fotografRepository.findByBrukernavn(user.getUsername());
+        f.setFotografId(fotgraf.getId());
         f.setTittel(file.getOriginalFilename());
         f.setContentType(file.getContentType());
+        f.setTittel(tittel);
+        f.setDato();
+        System.out.println(f.getTittel()+", "+f.getDato()+file.getSize());
         fotoRepository.save(f);
         System.out.println(f.getId());
         storageService.store(file, f.getId()+".jpg");
