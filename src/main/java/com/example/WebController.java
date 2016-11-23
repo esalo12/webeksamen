a1c3843f037ab9d7a770e4eaee874c4e61386b1f
esalo12 <espen.salomonsen@student.nord.no>
2016-11-24 00:09:59 +0100

    New changelist

--- src/main/java/com/example/WebController.java
+++ src/main/java/com/example/WebController.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import com.sun.deploy.security.UserDeclinedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,11 +23,6 @@ public class WebController {
         // Returnerer viewet "login.html" med map.addisjonen
         return "login";
     }
-    @RequestMapping(path="/nybruker", method = RequestMethod.GET)
-    public String nybruker(){
-        // Returnerer viewet "login.html" med map.addisjonen
-        return "nybruker";
-    }
 
     @RequestMapping(path="/login", method = RequestMethod.POST)
     public @ResponseBody String autoriser(){
@@ -30,12 +30,32 @@ public class WebController {
         return "rediger";
     }
 
-    @RequestMapping(path="/nybruker", method = RequestMethod.POST)
-    public String lagBruker(@RequestParam(value="fornavn") String fornavn, @RequestParam(value="etternavn") String etternavn,
-                            @RequestParam(value="brukernavn") String bruker, @RequestParam(value="passord") String passord){
-        Fotograf fotograf = new Fotograf(fornavn, etternavn, bruker, passord);
-        fotografRepository.save(fotograf);
-        return "Lagret";
+    @RequestMapping(path="/rediger", method = RequestMethod.GET)
+    public String rediger(){
+        // Returnerer viewet "rediger.html"
+        return "rediger";
     }
 
+    @RequestMapping(path="/nybruker", method = RequestMethod.GET)
+    public String nybruker(){
+        // Returnerer viewet "login.html" med map.addisjonen
+        return "nybruker";
+    }
+
+    @RequestMapping(path="/nybruker", method = RequestMethod.POST)
+    public ModelMap lagBruker(@RequestParam(value="fornavn") String fornavn, @RequestParam(value="etternavn") String etternavn,
+                                     @RequestParam(value="brukernavn") String bruker, @RequestParam(value="passord") String passord){
+        Fotograf user = fotografRepository.findByBrukernavn(bruker);
+        ModelMap map = new ModelMap();
+        if(user != null) {
+            map.addAttribute( "error", "nybruker");
+            return map;
+        }
+        else {
+            map.addAttribute("lagret", "nybruker");
+                        Fotograf fotograf = new Fotograf(fornavn, etternavn, bruker, passord);
+            fotografRepository.save(fotograf);
+            return map;
+        }
+    }
 }
