284f6863045a76b62127d5374e383d954f3e9ec5
esalo12 <espen.salomonsen@student.nord.no>
2016-12-01 20:59:04 +0100

    mer bugfix nytag som startet på <ødela layout

--- src/main/java/com/example/FotografController.java
+++ src/main/java/com/example/FotografController.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
+
 
 import java.util.List;
 
--- src/main/java/com/example/WebController.java
+++ src/main/java/com/example/WebController.java
@@ -2,17 +2,13 @@ package com.example;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
 import java.util.List;
 
